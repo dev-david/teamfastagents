41bb74db3b8488e7f952f8fedf1c2c3427ebe756
david <simpleaznx@gmail.com>
2013-09-26 16:03:56 -0700

    attr acc

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-  attr_accessible :Address, :Address2, :Bathrooms, :City, :Description, :Lot, :Number, :Rooms, :SqFt, :State, :Zip
+  attr_accessible :Address, :Address2, :Bathrooms, :City, :Description, :Lot, :Number, :Rooms, :SqFt, :State, :Zip, :price
   attr_accessible :picture
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
